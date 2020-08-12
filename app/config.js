98aa33032208a51184c144bd54e96cc2c0d1be61
keyaantravis2020 <keyaan.travis2020@outlook.com>
2020-08-13 02:27:41 +0700

    Initial Commit

---
+++ app/config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.version = '0.1.0';
+exports.id = 'net.freefinancetools.openfrombrowser';
+
+exports.ids = {
+  "chrome": [
+    "pnhphphdmklmlkhfajfppfgmpfpepohd", // Local Test ID
+  ],
+  "firefox": [
+    
+  ]
+};
